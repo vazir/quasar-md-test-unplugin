8781d8e6c893c259eec006772a8a4012383f112b
anton <anton.vazir@gmail.com>
2024-03-04 11:59:27 +0300

    Initial - 0.26.0

---
+++ src/pages/TestMD.vue
@@ -0,0 +1,30 @@
+<template>
+    <MD />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { computed } from 'vue';
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+import MD from 'src/markdown/mdtest.md'
+
+export default defineComponent({
+  name: 'TestMD',
+  components: {
+    MD,
+  },
+  setup () {
+    const route = useRoute()
+    const source = ref(null)
+    return {
+      source,
+    }
+  },
+  async beforeMount() {
+    console.log('Source: ', this.source)
+  },
+})
+</script>
+
